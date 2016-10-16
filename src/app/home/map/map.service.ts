6a78951734c69bc6e48042e501124982f5d70b82
B_i_p_y_C <ganiball54@gmail.com>
2016-10-16 17:46:51 +0300

    init maps 1

--- src/app/home/map/map.service.ts
+++ src/app/home/map/map.service.ts
@@ -10,8 +10,13 @@ export class MapService {
   setCoords(coords:Array, markers:Array) {
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
+      zoom: 10,
       center: {lat: coords[0], lng: coords[1]}
+    });
+    var marker = new google.maps.Marker({
+      position: {lat: coords[0], lng: coords[1]},
+      map: map,
+      title: 'Uluru (Ayers Rock)'
     });
     directionsDisplay.setMap(map);
   }
